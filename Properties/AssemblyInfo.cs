b3c99309c4a3b50d5ea95403c2abe7d6f9388468
Unknown <verner@fortelius.net>
2017-10-24 12:30:40 +0300

    testing

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,1 +1,2 @@
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("RadiusServer.Tests")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Flexinets.Radius.RadiusServer.Tests")]
